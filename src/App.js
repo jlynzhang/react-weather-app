7d3007f14779bf3757bf23a078d9dfa0ec01e520
jlynzhang <92702611+jlynzhang@users.noreply.github.com>
2022-03-31 01:23:56 -0500

    Add link to GitHub repo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        Project coded by Jess Zhang and open-sourced on{" "}
+        <a
+          href="https://github.com/jlynzhang/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
