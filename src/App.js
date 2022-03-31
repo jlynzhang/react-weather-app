c26ad691d4952745e7f8a3a85eec4ea64d9b8180
jlynzhang <92702611+jlynzhang@users.noreply.github.com>
2022-03-31 01:36:53 -0500

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Project coded by Jess Zhang and open-sourced on{" "}
-        <a
-          href="https://github.com/jlynzhang/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          Project coded by Jess Zhang and open-sourced on{" "}
+          <a
+            href="https://github.com/jlynzhang/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
